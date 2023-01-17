f7597c3b003f2bb2c7854effe8ccb04ee33f4606
fabianmeetsworld <fp.knasmueller@gmail.com>
2023-01-17 19:07:24 +0100

    #3 allerdings noch nicht durch Usereingabe

--- src/components/app-component.ts
+++ src/components/app-component.ts
@@ -5,8 +5,14 @@ import "./song/collection-table-component";
 
 //für den Nutzer sehbare Felder - View
 const template = html`
+    <!--<input type="radio" id="album" name="entity" value="album">
+    <label for="css">Album</label>
+    <input type="radio" id="song" name="entity" value="song">
+    <label for="css">Song</label><br>-->
+
     <label for="collection">Search:</label>
-    <input type="text" id="collection">
+    <input type="text" id="eingabe">
+
     <button value="Enter">Submit</button>
     <br><br>
 
@@ -24,7 +30,7 @@ class AppComponent extends HTMLElement {
         const input = this.shadowRoot.querySelector<HTMLInputElement>("input"); //Texteingabe des Users
         const button = this.shadowRoot.querySelector<HTMLButtonElement>("button"); //Submit-Button
         button.onclick = () => {
-            collectionService.search(input.value,"album");
+            collectionService.search(input.value,"song");
         };
     }
 }
