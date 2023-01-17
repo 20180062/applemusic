f90fea6f519405c71023398012cfc616e7901296
fabianmeetsworld <fp.knasmueller@gmail.com>
2023-01-17 14:54:19 +0100

    #1 testcommit

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,5 @@ title.textContent = "Applemusic Search";
 const body = document.querySelector("body");
 const appComponent = document.createElement("app-component"); //selbst erstelltes Element, macht im Endeffekt das gesamte Programm aus
 body.appendChild(appComponent); //app-component wird zum body hinzugefügt
+
+
